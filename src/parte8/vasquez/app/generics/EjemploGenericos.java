95fcaf224f4394385cbccf28a0a28b348f429b45
Peito1102 <renzitiwin@gmail.com>
2024-07-25 22:28:55 -0500

    parto 2,3 y 4 de generics

--- src/parte8/vasquez/app/generics/EjemploGenericos.java
+++ src/parte8/vasquez/app/generics/EjemploGenericos.java
@@ -1,6 +1,7 @@
 package parte8.vasquez.app.generics;
 
 import parte7.vasquez.app.modelo.Cliente;
+import parte7.vasquez.app.modelo.ClientePremium;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,9 +31,29 @@ public class EjemploGenericos {
         List<String> nombres = fromArrayToList(new String[]{"JD","PEPITO","CAJITA"}, enteros);
         System.out.println();
         nombres.forEach(System.out::println);
+
+        List<ClientePremium> clientesPremiums = fromArrayToList(new ClientePremium[]{new ClientePremium("Jona","Iñoñan")});
+        imprimirClientes(clientes);
+        System.out.println();
+        imprimirClientes(clientes2);
+        System.out.println();
+        imprimirClientes(clientesPremiums);
+
+        System.out.println("Maximo de 1,4 y 9 :" + maximo(1,9,4));
+        System.out.println("Maximo de 3.9, 11.6 y 7.80 :" + maximo(3.9,11.6,7.80));
+        System.out.println("Maximo de zanahoria, esparrago y alberja : " + maximo("zanahoria", "esparrago","alberja"));
+
     }
 
     public static <T> List<T> fromArrayToList(T [] c) {
+        return Arrays.asList(c);
+    }
+
+    public static <T extends Number> List<T> fromArrayToList(T [] c) {
+        return Arrays.asList(c);
+    }
+
+    public static <T extends Cliente & Comparable<T>> List<T> fromArrayToList(T [] c) {
         return Arrays.asList(c);
     }
 
@@ -41,6 +62,21 @@ public class EjemploGenericos {
             System.out.println(elemento);
         }
         return Arrays.asList(c);
+    }
+
+    public static void imprimirClientes(List<? extends Cliente> clientes) {
+        clientes.forEach(System.out::println);
+    }
+
+    public static <T extends Comparable<T>> T maximo(T a, T b, T c) {
+        T max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        return max;
     }
 
 }
